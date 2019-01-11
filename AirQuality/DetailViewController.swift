58952ac3dc97c8ab6585631ff198fe075b9874b5
Thomas Feng <feng.thom@gmail.com>
2019-01-11 17:23:44 +0900

    Added DetailViewController

---
+++ AirQuality/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  AirQuality
+//
+//  Created by Thomas Feng on 1/11/19.
+//  Copyright © 2019 Infinity and Beyond. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
