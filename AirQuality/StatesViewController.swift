5f7b35730438720b94425c65cd4ddc5e25d3433c
Thomas Feng <feng.thom@gmail.com>
2019-01-11 17:10:29 +0900

    Added States and Cities View Controllers

---
+++ AirQuality/StatesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StatesViewController.swift
+//  AirQuality
+//
+//  Created by Thomas Feng on 1/11/19.
+//  Copyright © 2019 Infinity and Beyond. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StatesViewController: UITableViewController {
+    
+}
