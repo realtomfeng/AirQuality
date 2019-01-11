5f80a5f748a0b3396fdf368b3eddac7cd99c9d9e
Thomas Feng <feng.thom@gmail.com>
2019-01-11 16:20:01 +0900

    added TrackedCitiesTableView Controller

---
+++ AirQuality/TrackCitiesTableViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TrackCitiesTableViewController.swift
+//  AirQuality
+//
+//  Created by Thomas Feng on 1/11/19.
+//  Copyright © 2019 Infinity and Beyond. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrackedCitiesTableViewController: UITableViewController {
+    
+}
