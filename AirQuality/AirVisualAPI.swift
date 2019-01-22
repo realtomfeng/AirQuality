3430d5e3cead5b344ca157f4c3bff08fa6b2e6ae
Thomas Feng <feng.thom@gmail.com>
2019-01-22 15:48:37 +0900

    Added Air Visual API

--- AirQuality/AirVisualAPI.swift
+++ AirQuality/AirVisualAPI.swift
@@ -8,30 +8,105 @@
 
 import Foundation
 
+enum APIError: Error {
+    case couldNotParseJSON
+}
+
 struct AirVisualAPI {
-    private static var baseURLString = "http://api.airvisual.com/v2/"
+    private static let baseURLString = "https://api.airvisual.com/v2/"
     private static let apiKey = "gDpZQhYxdvxHvSTzB"
 
+    static func countriesData(fromJSON data: Data) -> CountriesResult {
+        do {
+            var allCountries = [String]()
+            
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            let countriesData = json["data"] as! [[String: String]]
+            
+            for country in countriesData {
+                guard let countryName = country["country"] else { continue }
+                allCountries.append(countryName)
+            }
+            
+            return .success(allCountries)
+        } catch {
+            return .failure(error)
+        }
+    }
+
+    static func statesData(fromJSON data: Data) -> StatesResult {
+        do {
+            var allStates = [String]()
+            
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            let statesData = json["data"] as! [[String: String]]
+            
+            for state in statesData {
+                guard let stateName = state["state"] else { continue }
+                allStates.append(stateName)
+            }
+            
+            return .success(allStates)
+        } catch {
+            return .failure(error)
+        }
+    }
+
+    static func citiesData(fromJSON data: Data) -> CitiesResult {
+        do {
+            var allCities = [String]()
+            
+            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            guard let citiesData = json["data"] as? [[String: String]] else {
+                let error = json["data"] as? [String:String]
+                return .cityNotFound((error?["message"])!)
+            }
+            
+            for city in citiesData {
+                guard let cityName = city["city"] else { continue }
+                allCities.append(cityName)
+            }
+            
+            return .success(allCities)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+    static func airQualityData(fromJSON data: Data) -> AirQualityResult {
+        do {
+            guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                let jsonData = json["data"] as? [String: Any] else {
+                    return .failure(APIError.couldNotParseJSON)
+            }
+            if let airQuality = AirQuality(json: jsonData) {
+                return .success(airQuality) } else { return .failure(APIError.couldNotParseJSON) }
+        } catch {
+            return .failure(error)
+        }
+    }
+    
     static func airVisualURL(country: String?, state: String?, city: String?) -> URL {
+        var url = baseURLString
         var queryItems = [URLQueryItem]()
         var baseParams = [
             "key": apiKey,
         ]
         switch country {
         case nil:
-            baseURLString.append("country?")
+            url.append("countries?")
         default:
+            baseParams["country"] = country
             switch state {
             case nil:
-                baseURLString.append("state?")
-                baseParams["country"] = country
+                url.append("states?")
             default:
+                baseParams["state"] = state
                 switch city {
                 case nil:
-                    baseURLString.append("city?")
-                    baseParams["state"] = state
+                    url.append("cities?")
                 default:
-                    baseParams["state"] = state
+                    url.append("city?")
                     baseParams["city"] = city
                 }
             }
@@ -42,8 +117,10 @@ struct AirVisualAPI {
             queryItems.append(item)
         }
         
-        var components = URLComponents(string: baseURLString)!
+        var components = URLComponents(string: url)!
         components.queryItems = queryItems
+        print(components.url)
+
         return components.url!
     }
 }
