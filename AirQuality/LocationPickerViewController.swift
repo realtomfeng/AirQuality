84b7fbc6a55c46a459aa9ce0becb70ea22c20aa0
Thomas Feng <feng.thom@gmail.com>
2019-01-22 15:51:06 +0900

    Added a navigation controller with view controllers that can be added and removed to add a city to track

---
+++ AirQuality/LocationPickerViewController.swift
@@ -0,0 +1,118 @@
+//
+//  LocationPickerViewController.swift
+//  AirQuality
+//
+//  Created by Thomas Feng on 1/14/19.
+//  Copyright © 2019 Infinity and Beyond. All rights reserved.
+//
+
+import UIKit
+
+protocol LocationPickerViewControllerDelegate {
+    func didSelect(country: String, state: String, city: String)
+}
+
+final class LocationPickerViewController: UINavigationController, StringPickerTableViewControllerDelegate {
+    
+    var pickerDelegate: LocationPickerViewControllerDelegate?
+    
+    
+    var country: String?
+    var state: String?
+    var city: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // pick country
+        //      get countries
+        //      show picker
+        //      get picked country
+        
+        // pick state
+        
+        // pick city
+        
+        showCountryPicker()
+    }
+    
+    private func showCountryPicker() {
+        AirQualityStore.shared.fetchAvailableCountries { (result) -> Void in
+            switch result {
+            case let .success(countries):
+                let picker = StringPickerTableViewController(titles: countries, delegate: self)
+                DispatchQueue.main.async {
+                    self.pushViewController(picker, animated: true)
+                }
+            case let .failure(error):
+                print("Error fetching request with error: \(error)")
+
+            }
+        }
+    }
+
+    private func showStatePicker() {
+        guard let country = country else { return }
+        AirQualityStore.shared.fetchAvailableStates(in: country) { (result) -> Void in
+            switch result {
+            case let .success(states):
+                let picker = StringPickerTableViewController(titles: states, delegate: self)
+                DispatchQueue.main.async {
+                    self.pushViewController(picker, animated: true)
+                }
+            case let .failure(error):
+                print("Error fetching request with error: \(error)")
+                
+            }
+        }
+    }
+
+    private func showCitiesPicker() {
+        guard let country = country, let state = state else { return }
+        AirQualityStore.shared.fetchAvailableCitiesIn(state: state, country: country) { (result) -> Void in
+            switch result {
+            case let .success(cities):
+                let picker = StringPickerTableViewController(titles: cities, delegate: self)
+                DispatchQueue.main.async {
+                    self.pushViewController(picker, animated: true)
+                }
+            case let .failure(error):
+                print("Error fetching request with error: \(error)")
+            case let .cityNotFound(string):
+                let alert = UIAlertController(title: "Error", message: string, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in self.dismiss(animated: true, completion: nil) } ))
+                self.present(alert, animated: true, completion: nil)
+                print(string)
+            }
+        }
+    }
+    
+    private func addCity() {
+        guard let country = country, let state = state, let city = city else { return }
+         AirQualityStore.shared.fetchAirQualityFor(city: city, state: state, country: country) {
+            (airQualityResult) -> Void in
+            switch airQualityResult {
+            case let .success(airQuality):
+                TrackedCitiesDataSource.shared.allTrackedCities.append(airQuality)
+                
+            case let .failure(error):
+                print("Got error", error)
+            }
+        }
+    }
+    
+    
+    func stringPickerDidSelect(picker: StringPickerTableViewController, title: String) {
+        if country == nil {
+            country = title
+            showStatePicker()
+        } else if state == nil {
+            state = title
+            showCitiesPicker()
+        } else {
+            city = title
+            addCity()
+            dismiss(animated: true, completion: nil)
+        }
+    }
+}
---
+++ AirQuality/StringPickerTableViewController.swift
@@ -0,0 +1,79 @@
+//
+//  StringPickerTableViewController.swift
+//  AirQuality
+//
+//  Created by Thomas Feng on 1/14/19.
+//  Copyright © 2019 Infinity and Beyond. All rights reserved.
+//
+
+import UIKit
+
+protocol StringPickerTableViewControllerDelegate {
+    func stringPickerDidSelect(picker: StringPickerTableViewController, title: String)
+}
+
+class StringPickerTableViewController: UITableViewController, UISearchBarDelegate {
+    
+    private let titles: [String]
+    private var filteredTitles: [String]
+    private let delegate: StringPickerTableViewControllerDelegate?
+    lazy var searchBar:UISearchBar = UISearchBar()
+    
+    init(titles: [String], delegate: StringPickerTableViewControllerDelegate) {
+        self.titles = titles
+        self.delegate = delegate
+        self.filteredTitles = titles
+        super.init(nibName: nil, bundle: nil)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+    }
+    
+    override func viewDidLoad()
+    {
+        searchBar.searchBarStyle = UISearchBar.Style.prominent
+        searchBar.placeholder = " Search..."
+        searchBar.sizeToFit()
+        searchBar.isTranslucent = false
+        searchBar.backgroundImage = UIImage()
+        searchBar.delegate = self
+        navigationItem.titleView = searchBar
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.filteredTitles.removeAll()
+        if searchText.count != 0 {
+            for title in titles {
+                let range = title.lowercased().range(of: searchText, options: .caseInsensitive, range: nil, locale: nil)
+                if range != nil {
+                    self.filteredTitles.append(title)
+                }
+            }
+        }
+        tableView.reloadData()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK - Data Source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredTitles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = filteredTitles[indexPath.row]
+        
+        return cell
+    }
+    
+    // MARK - Delegate
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.stringPickerDidSelect(picker: self, title: titles[indexPath.row])        
+    }
+    
+}
