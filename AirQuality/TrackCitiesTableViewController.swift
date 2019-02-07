c2a1aaab97278c62c00d048bc92a0b187e480284
Thomas Feng <feng.thom@gmail.com>
2019-02-07 15:40:51 +0900

    Added Tracked Cities View Controller

--- AirQuality/TrackCitiesTableViewController.swift
+++ AirQuality/TrackCitiesTableViewController.swift
@@ -11,4 +11,49 @@ import UIKit
 
 class TrackedCitiesTableViewController: UITableViewController {
     
+    let api = AirQualityStore()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(forName: .citiesDidUpdate, object: nil, queue: nil) { _ in
+            self.tableView.reloadData()
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return TrackedCitiesDataSource.shared.allTrackedCities.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = TrackedCitiesDataSource.shared.allTrackedCities[indexPath.row].city
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "showAirQualityForCity"?:
+            if let row = tableView.indexPathForSelectedRow?.row {
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.airQuality = TrackedCitiesDataSource.shared.allTrackedCities[row]
+            }
+        case "searchForCity"?:
+            _ = segue.destination as! LocationPickerViewController
+        default:
+            preconditionFailure("Unexpect Segue identifier")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            TrackedCitiesDataSource.shared.allTrackedCities.remove(at: indexPath.row)
+        }
+    }
+
 }
