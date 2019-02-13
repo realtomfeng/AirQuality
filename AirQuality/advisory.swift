ea039e0299899020c852a07638166d9a680e3976
Thomas Feng <feng.thom@gmail.com>
2019-02-13 16:41:58 +0900

    Added Advisory enum

---
+++ AirQuality/advisory.swift
@@ -0,0 +1,13 @@
+//
+//  advisory.swift
+//  AirQuality
+//
+//  Created by Thomas Feng on 2/13/19.
+//  Copyright © 2019 Infinity and Beyond. All rights reserved.
+//
+
+import Foundation
+
+enum advisory {
+    
+}
